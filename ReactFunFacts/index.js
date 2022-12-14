f0f0e15e57c89cdee68c625455be9eb091e3df34
Max <104834753+MaximEsin@users.noreply.github.com>
2022-12-14 11:20:35 +0700

    React fun facts start

---
+++ ReactFunFacts/index.js
@@ -0,0 +1,11 @@
+const Navbar = (
+  <nav>
+    <h1>Website</h1>
+    <ul>
+      <li>About</li>
+      <li>Pricing</li>
+      <li>Contacts</li>
+    </ul>
+  </nav>
+);
+ReactDOM.render(Navbar, document.getElementById("root"));
