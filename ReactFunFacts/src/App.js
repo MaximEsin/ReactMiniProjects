dbc516c9b36935936fc5118be5570bb6e2cde0b8
Max <104834753+MaximEsin@users.noreply.github.com>
2022-12-14 11:46:04 +0700

    Reorganised

---
+++ ReactFunFacts/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h1>Website</h1>
+        <ul>
+          <li>About</li>
+          <li>Pricing</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
